55d725e950e7b7054f7e527c439103670106c873
nikitha2810 <110811676+nikitha2810@users.noreply.github.com>
2023-11-09 17:53:02 +0530

    Update Home.js

--- src/Layout/Home.js
+++ src/Layout/Home.js
@@ -16,31 +16,31 @@ export default function componentName() {
           <th>1.</th>
           <th>Paragraph Word Counter</th>
           <th><Link to='p1'>Project 1</Link></th>
-          <th><a href="https://github.com/Yaswanth1320/Reactjs">Project-1</a></th>
+          <th><a href="https://github.com/nikitha2810/react-all">Project-1</a></th>
         </tr>
         <tr>
           <th>2.</th>
           <th>Calculator</th>
           <th><Link to='p2'>Project 2</Link></th>
-          <th><a href="https://github.com/Yaswanth1320/Reactjs">Project-2</a></th>
+          <th><a href="https://github.com/nikitha2810/react-all">Project-2</a></th>
         </tr>
         <tr>
           <th>3.</th>
           <th>Color Picker</th>
           <th><Link to='p3'>Project 3</Link></th>
-          <th><a href="https://github.com/Yaswanth1320/Reactjs">Project-3</a></th>
+          <th><a href="https://github.com/nikitha2810/react-all">Project-3</a></th>
         </tr>
         <tr>
           <th>4.</th>
           <th>Api fetch (Dummy data)</th>
           <th><Link to='p4'>Project 4</Link></th>
-          <th><a href="https://github.com/Yaswanth1320/Reactjs">Project-4</a></th>
+          <th><a href="https://github.com/nikitha2810/react-all">Project-4</a></th>
         </tr>
         <tr>
           <th>5.</th>
           <th>Age Calculator</th>
           <th><Link to='p5'>Project 5</Link></th>
-          <th><a href="https://github.com/Yaswanth1320/Reactjs">Project-5</a></th>
+          <th><a href="https://github.com/nikitha2810/react-all">Project-5</a></th>
         </tr>
 
       </table>
